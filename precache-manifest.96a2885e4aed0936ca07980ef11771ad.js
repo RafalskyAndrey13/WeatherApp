4af076a6972bf6e14ea2fffaab80690addceae25
RafalskyAndrey13 <mycourses13@gmail.com>
2019-11-12 11:18:26 +0200

    Updates

---
+++ precache-manifest.96a2885e4aed0936ca07980ef11771ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a50c0ac5d19338b9065c706707f8c73",
+    "url": "/WeatherIt/index.html"
+  },
+  {
+    "revision": "6f4c2d4167b14a2c4e22",
+    "url": "/WeatherIt/static/css/2.2ebd2838.chunk.css"
+  },
+  {
+    "revision": "c1b33c406842119cef68",
+    "url": "/WeatherIt/static/css/main.79beb295.chunk.css"
+  },
+  {
+    "revision": "6f4c2d4167b14a2c4e22",
+    "url": "/WeatherIt/static/js/2.369773a3.chunk.js"
+  },
+  {
+    "revision": "c1b33c406842119cef68",
+    "url": "/WeatherIt/static/js/main.16d251e4.chunk.js"
+  },
+  {
+    "revision": "4835506a50c1fab9bdf7",
+    "url": "/WeatherIt/static/js/runtime-main.b2b57d18.js"
+  },
+  {
+    "revision": "67b363f4a22310663a77b33619213a60",
+    "url": "/WeatherIt/static/media/preloader.67b363f4.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/WeatherIt/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/WeatherIt/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/WeatherIt/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/WeatherIt/static/media/slick.f97e3bbf.svg"
+  }
+]);
